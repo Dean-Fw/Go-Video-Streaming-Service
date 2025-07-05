b4758f6fc8a549a7f334ee5e7df1472ce081673f
dfw <dfw662@gmail.com>
2025-07-05 21:03:59 +0100

    Initial implementation

---
+++ Backend/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"math"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func videoHandler(w http.ResponseWriter, r *http.Request) {
+
+	video := strings.Split(r.URL.Path, "/")[2]
+
+	requestedBytes := r.Header.Get("Range")
+
+	bytesRange := strings.Split(requestedBytes, "=")[1]
+	bytesStartAndEnd := strings.Split(bytesRange, "-")
+
+	intStart, err := strconv.Atoi(bytesStartAndEnd[0])
+
+	if err != nil {
+		fmt.Fprint(w, "Failed to read range header")
+	}
+
+	amountOfBytes := math.Pow(10, 6)
+
+	file, err := GetFile(video)
+
+	if err == os.ErrNotExist {
+		fmt.Fprintf(w, "404 Could not find file: %s.mp4\n", video)
+		return
+	} else if err != nil {
+		log.Fatalf("Failed to read file: %s", err.Error())
+	}
+
+	bytes := make([]byte, int(amountOfBytes))
+
+	fileInfo, _ := file.Stat()
+
+	file.ReadAt(bytes, int64(intStart))
+
+	end := int(math.Min(float64(intStart)+amountOfBytes, float64(fileInfo.Size()-1)))
+
+	w.Header().Add("Content-Type", "video/mp4")
+	w.Header().Add("Content-Range", fmt.Sprintf("bytes %d-%d/%d", intStart, end, fileInfo.Size()))
+	w.Header().Add("Accept-Ranges", "bytes")
+
+	w.WriteHeader(206)
+
+	w.Write(bytes)
+
+	file.Close()
+
+}
+
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.ParseFiles("../Templates/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	temp.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("GET /video/{name}", videoHandler)
+
+	http.HandleFunc("GET /{$}", homePageHandler)
+
+	log.Print("[SERVER] : Running on Port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
